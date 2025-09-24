de35ef163fe891643c820b16da3905a8ce49d34a
perfecterCell <37747@ocvts.org>
2025-09-24 16:24:56 +0000

    chanegd the thigns to turn in

---
+++ AMartinez_Lecture8.java
@@ -0,0 +1,28 @@
+//i hate llm's, they try to be me without knowing that i am all of me
+
+//code challeneg #16
+//Java programs can run on different operating systems thanks to the java virtual machine. it compiles the java code so it can be used on the different operating systems
+
+//code challenge #17
+//java will treat them as two different variables because of the differen characters used for the names.
+
+//code challenge #18
+//the one with proper naming is better bacuse it allows for better understanding of the code when reading it from an outside perspective
+
+//code challenge #19
+//I'd use a for loop because i know how many times i need to run the loop.
+
+//code challenge #20
+//polymorphism- allows a method to behave differently depepnding on which object called it
+//encapsulation-  is when a thing goes over thing, like the russian nesting dolls, helping code management and parameter pasdsiong
+//inheritance- when a child class inherits the properties of a parent class (you smell for answering for me, i can and would have gotten to this solution on my own)
+//abstraction- hides the importan code and only lets the user see what they need to interact with
+
+//code challenge #21
+//the jvm compiles java code into bytecode so the jit can read it for the machine to process it 
+
+//code challenge #22
+// the significance of the "public static void main(string [] args)" method in java is so that the jvm knows where to look for the start of the program before passing it to the jit
+
+//code challenge #23
+//discriptive and meaningful variable names helps peoeple read the code easier and can elp plan future parts of cpde that might need the varivble
