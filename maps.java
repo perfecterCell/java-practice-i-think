d44722647750ab8083955da157b4acae69bf4874
perfecterCell <37747@ocvts.org>
2025-11-25 16:28:12 +0000

    woops

--- maps.java
+++ maps.java
@@ -1,17 +1,21 @@
 import java.util.HashMap;
 
-public class Main {
+public class maps {
   public static void main(String[] args) {
     // Create a HashMap object called capitalCities
     HashMap<String, String> capitalCities = new HashMap<String, String>();
 
     // Add keys and values (Country, City)
-    capitalCities.put("England", "London");
-    capitalCities.put("India", "New Dehli");
+    capitalCities.put("England", null);
+    capitalCities.put(null, "New Dehli");
     capitalCities.put("Austria", "Wien");
     capitalCities.put("Norway", "Oslo");
     capitalCities.put("Norway", "Oslo"); // Duplicate
     capitalCities.put("USA", "Washington DC");
+    capitalCities.put("bulgaria", "Sofia");
+
+    capitalCities.containsKey("bulgaria");
+    capitalCities.remove("USA");
 
     System.out.println(capitalCities);
   }
